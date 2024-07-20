24cf2141989f79b48413c16a45dd3cf97e8fb5bd
Scottify20 <scottify20@gmail.com>
2024-07-20 18:20:25 +0800

    Added save scrolling position feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,43 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, Inject, OnInit, OnDestroy } from '@angular/core';
+import { Router, NavigationStart, RouterOutlet } from '@angular/router';
+import { filter } from 'rxjs/operators';
+import { ScrollPositionService } from './shared/services/scroll-position.service';
+import { DOCUMENT } from '@angular/common';
 import { NavComponent } from './nav/nav.component';
-import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavComponent, NgIf],
+  imports: [RouterOutlet, NavComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'Salida';
+  excludeRoutes: string[] = ['/lists']; // excluded routes for scroll position saving
+
+  constructor(
+    private router: Router,
+    private scrollPositionService: ScrollPositionService,
+    @Inject(DOCUMENT) private document: Document
+  ) {
+    this.router.events
+      .pipe(
+        filter(
+          (event) =>
+            event instanceof NavigationStart &&
+            event.navigationTrigger !== 'popstate' && // Navbar clicks
+            !this.excludeRoutes.includes(event.url) // Exclude specified routes
+        )
+      )
+      .subscribe(() => {
+        const mainElement = this.document.querySelector('main');
+        if (mainElement) {
+          this.scrollPositionService.savePosition(
+            this.router.url,
+            mainElement.scrollTop // Save main's scroll
+          );
+        }
+      });
+  }
 }
--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,30 +1,22 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
+import { ScrollPositionService } from '../shared/services/scroll-position.service';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'nav',
   standalone: true,
   imports: [CommonModule, RouterModule],
   templateUrl: './nav.component.html',
-  styleUrl: './nav.component.scss',
+  styleUrls: ['./nav.component.scss'],
 })
 export class NavComponent {
-  constructor(private router: Router) {}
-
-  isActive(route: string): boolean {
-    return this.router.url === route;
-  }
-
-  getIconPath(navItemIndex: number): string {
-    const navItem = this.navItems[navItemIndex];
-    const route = navItem.routerPath;
-
-    return this.isActive(route)
-      ? navItem.iconSvgPathSolid
-      : navItem.iconSvgPathOutline;
-  }
-
+  constructor(
+    private router: Router,
+    private scrollPositionService: ScrollPositionService,
+    @Inject(DOCUMENT) private document: Document
+  ) {}
   navItems: {
     label: string;
     iconSvgPathSolid: string;
@@ -54,4 +46,29 @@ export class NavComponent {
       exact: false,
     },
   ];
+
+  isActive(route: string): boolean {
+    return this.router.url === route;
+  }
+
+  getIconPath(navItemIndex: number): string {
+    const navItem = this.navItems[navItemIndex];
+    const route = navItem.routerPath;
+
+    return this.isActive(route)
+      ? navItem.iconSvgPathSolid
+      : navItem.iconSvgPathOutline;
+  }
+
+  navigateToRoute(route: string): void {
+    const mainElement = this.document.querySelector('main');
+    if (mainElement) {
+      this.scrollPositionService.savePosition(
+        this.router.url,
+        mainElement.scrollTop
+      );
+    }
+
+    this.router.navigateByUrl(route);
+  }
 }
