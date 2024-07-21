a3975db9b00dad99384849371e0ccb02ab3c1718
Scottify20 <scottify20@gmail.com>
2024-07-21 14:48:09 +0800

    Enabled built-in scroll position restoration feature

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,18 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import {
+  provideRouter,
+  withEnabledBlockingInitialNavigation,
+} from '@angular/router';
+import { routes } from './app/app.routes';
+import { withInMemoryScrolling } from '@angular/router';
 
-bootstrapApplication(AppComponent, appConfig).catch((err) =>
-  console.error(err)
-);
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(
+      routes,
+      withEnabledBlockingInitialNavigation(),
+      withInMemoryScrolling({ scrollPositionRestoration: 'enabled' })
+    ),
+  ],
+}).catch((err) => console.error(err));
